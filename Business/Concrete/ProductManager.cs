7345f87efcee823dc5f6f0b7bf4443c489fe280b
Nurhayat <69644512+nurhayatketenci@users.noreply.github.com>
2021-02-18 00:09:38 +0300

    18.02

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -23,7 +24,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        [Validate]
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             _productDal.Add(product);
